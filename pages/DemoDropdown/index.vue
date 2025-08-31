62ef75432bc941718d284e126bc2a569fdea06a7
Vhoang1015 <vhoang1015@gmail.com>
2025-09-01 00:22:15 +0700

    add demoDropdown page

---
+++ pages/DemoDropdown/index.vue
@@ -0,0 +1,59 @@
+<script>
+import { defineComponent } from "vue"
+import BaseDropdownComponent from "~/components/Dropdown.vue"
+import Button from "~/components/Button.vue"
+export default defineComponent({
+  name: "DemoDropdown",
+
+  components: {
+    BaseDropdownComponent,
+    Button
+  },
+})
+</script>
+
+<template>
+  <div class="page">
+    <!-- Basic Dropdown -->
+    <section class="demo-section">
+      <h2>Basic Dropdown</h2>
+      <BaseDropdownComponent>
+        <template #trigger="{ isOpen = !isOpen }">
+          <Button variant="primary">
+            Menu
+          </Button>
+        </template>
+
+        <template #content>
+          <div
+            onclick="location.href='http://localhost:3000/DemoModal'"
+            class="dropdown__item"
+          >
+            DemoModal
+          </div>
+          <div
+            onclick="location.href='#'"
+            class="dropdown__item"
+          >
+            Action 2
+          </div>
+          <div
+            onclick="location.href='#'"
+            class="dropdown__item"
+          >
+            Action 3
+          </div>
+        </template>
+      </BaseDropdownComponent>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  display: flex;
+
+  padding-inline: var(--size-space-medium);
+  padding-block: var(--size-space-medium);
+}
+</style>
