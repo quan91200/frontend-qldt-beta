e80ce26f974e7fa95e3c6cc257711860e708af7b
quan91200 <xb91200@gmail.com>
2025-09-03 20:58:24 +0700

    build default layout

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<script>
+import Sidebar from '~/components/Layout/Sidebar.vue'
+
+import Header from '~/components/Layout/Header.vue'
+
+export default {
+  name: 'DefaultLayout',
+
+  components: {
+    Sidebar,
+    Header,
+  },
+}
+</script>
+
+<template>
+  <div class="unit-container">
+    <Header />
+
+    <div class="unit-content">
+      <Sidebar />
+
+      <main class="unit-main">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style>
+.unit-container {
+
+  color: var(--color-zinc-900);
+  background-color: var(--color-zinc-50);
+}
+</style>
+
+<style scoped>
+.unit-content {
+  flex-grow: 1;
+  display: flex;
+  min-width: 0;
+}
+
+.unit-content > .unit-main {
+  margin-block-end: var(--size-space-large);
+  margin-inline-end: var(--size-space-large);
+
+  padding: var(--size-space-large);
+  border-radius: var(--size-border-radius-small);
+
+  flex-grow: 1;
+
+  min-height: calc(100dvh - var(--size-header-height) - 1rem);
+  background-color: var(--color-gray-200);
+
+  overflow: auto;
+}
+</style>
