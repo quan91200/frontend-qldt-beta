9e0212ac64a19aaaecb643d32990fec39469657b
quan91200 <xb91200@gmail.com>
2025-09-09 12:45:36 +0700

    demo page breadcrumb

---
+++ pages/DemoBreadcrumb/users/index.vue
@@ -0,0 +1,42 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import {
+  NuxtLink,
+} from '#components'
+
+import Breadcrumb from '~/components/Breadcrumb.vue'
+
+export default defineComponent({
+  components: {
+    NuxtLink,
+    Breadcrumb,
+  },
+})
+</script>
+
+<template>
+  <div class="unit-demo-breadcrumb">
+    <Breadcrumb />
+
+    <h1>Settings Page</h1>
+    <NuxtLink to="/DemoBreadcrumb">
+      Back to Breadcrumb
+    </NuxtLink>
+
+    <NuxtLink to="/DemoBreadcrumb/users/settings">
+      Go to Settings
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.unit-demo-breadcrumb {
+  display: flex;
+  flex-direction: column;
+
+  gap: var(--size-space-large);
+}
+</style>
---
+++ pages/DemoBreadcrumb/users/settings/index.vue
@@ -0,0 +1,38 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import {
+  NuxtLink,
+} from '#components'
+
+import Breadcrumb from '~/components/Breadcrumb.vue'
+
+export default defineComponent({
+  components: {
+    NuxtLink,
+    Breadcrumb,
+  },
+})
+</script>
+
+<template>
+  <div class="unit-demo-breadcrumb">
+    <Breadcrumb />
+
+    <h1>Settings Page</h1>
+    <NuxtLink to="/DemoBreadcrumb/users">
+      Back to Users
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.unit-demo-breadcrumb {
+  display: flex;
+  flex-direction: column;
+
+  gap: var(--size-space-large);
+}
+</style>
